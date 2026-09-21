2d109829265b21f9abd98859b7eac0c413cd85ac
Camilo Molina <brayancamilomolinadev@gmail.com>
2026-09-21 16:26:53 -0500

    #Fix-0001 Changes Icon and add  LOGO

--- src/components/HomeMediaCarousel.jsx
+++ src/components/HomeMediaCarousel.jsx
@@ -4,6 +4,10 @@ import serviceImageThree from '../assets/carousel/Imagen_APH_2.jpeg'
 import serviceImageOne from '../assets/carousel/Imagen_APH_3.jpeg'
 import serviceImageTwo from '../assets/carousel/Imagen_APH_5.jpeg'
 import serviceImageFour from '../assets/carousel/Imagen_APH_4.jpeg'
+import serviceImageFive from '../assets/carousel/Imagen_Logistic_1.jpeg'
+import serviceImageSix from '../assets/carousel/Imagen_Logistic_2.jpeg'
+import serviceImageSeven from '../assets/carousel/Imagen_Logistic_3.jpeg'
+
 
 
 const MEDIA_ITEMS = [
@@ -12,6 +16,9 @@ const MEDIA_ITEMS = [
   { url: generatedImage, type: 'image', title: 'Imagen destacada' },
   { url: serviceImageThree, type: 'image', title: 'Imagen de capacitación' },
   { url: serviceImageFour, type: 'image', title: 'Imagen de capacitación adicional' },
+  { url: serviceImageFive, type: 'image', title: 'Imagen de logística 1' },
+  { url: serviceImageSix, type: 'image', title: 'Imagen de logística 2' },
+  { url: serviceImageSeven, type: 'image', title: 'Imagen de logística 3' },
 ]
 
 function HomeMediaCarousel() {
